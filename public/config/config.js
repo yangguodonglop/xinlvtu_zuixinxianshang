ca84ca9bbd23069e8cc4e18a5c86abab8f86434f
yangguodonglop <362960082@qq.com>
2018-08-18 17:27:37 +0800

    2018/8/18

---
+++ public/config/config.js
@@ -0,0 +1,30 @@
+URL = 'http://mapi.xinlv123.com/xltx';
+URLS= 'http://mapi.xinlv123.com/xltx/mobile';
+SCORDERURL = 'http://mapi.xinlv123.com/xltx/mobile';
+goods_details ='http://mapi.xinlv123.com/xltx/mobile/goods/details';
+order_fill ='http://mapi.xinlv123.com/xltx/mobile/order';
+
+
+
+index_list='http://mapi.xinlv123.com/xltx/mobile/getDataList';
+
+// function set(key,value){
+//     var curTime = new Date().getTime();
+//     localStorage.setItem(key,JSON.stringify({data:value,time:curTime}));
+// }
+//
+// function get(key,exp){
+//     var data = localStorage.getItem(key);
+//     var dataObj = JSON.parse(data);
+//     if (new Date().getTime() - dataObj.time>exp) {
+//         console.log('信息已过期');
+//         //alert("信息已过期")
+//     }else{
+//         //console.log("data="+dataObj.data);
+//         //console.log(JSON.parse(dataObj.data));
+//         var dataObjDatatoJson = JSON.parse(dataObj.data)
+//         return dataObjDatatoJson;
+//     }
+// }
+
+
